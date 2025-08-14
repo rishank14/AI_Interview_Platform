31edec460b268f880b8f5289c8c3565f93a9a9a2
rishank14 <rishankkalra@gmail.com>
2025-08-14 12:25:28 +0530

    feat: update dependencies and remove unused utility functions
    - Added new dependencies: @ai-sdk/google, @vapi-ai/web, ai, dayjs, firebase, and firebase-admin.
    - Updated TypeScript definitions in index.d.ts and vapi.d.ts to ensure consistency.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,7 @@
 import AuthForm from "@/components/AuthForm";
-import React from "react";
 
-const page = () => {
-   return <AuthForm type="sign-up" />;
+const Page = () => {
+   return <AuthForm type="sign-in" />;
 };
 
-export default page;
+export default Page;
